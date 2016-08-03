dd13e8d21bbc409258056e5bf4c4d51cbfadaa64
you8023 <651415395@qq.com>
2016-08-03 16:01:52 +0800

    gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,56 @@
+//导入工具包 require('node_modules里对应模块')
+// var gulp = require('gulp'), //本地安装gulp所用到的地方
+//     less = require('gulp-less');
+ 
+// //定义一个testLess任务（自定义任务名称）
+// gulp.task('testLess', function () {
+//     gulp.src('index.less') //该任务针对的文件
+//         .pipe(less()) //该任务调用的模块
+//         .pipe(gulp.dest('css')); //将会在src/css下生成index.css
+// });
+ 
+// gulp.task('default',['testLess']); //定义默认任务 elseTask为其他任务，该示例没有定义elseTask任务
+ 
+//gulp.task(name[, deps], fn) 定义任务  name：任务名称 deps：依赖任务名称 fn：回调函数
+//gulp.src(globs[, options]) 执行任务处理的文件  globs：处理的文件路径(字符串或者字符串数组) 
+//gulp.dest(path[, options]) 处理完后文件生成路径
+//
+var gulp = require('gulp');
+var jshint = require('gulp-jshint');//语法检查
+var concat = require('gulp-concat');//合并文件
+var uglify = require('gulp-uglify');//压缩代码
+var rename = require('gulp-rename');//重命名
+var less = require('gulp-less');//less转换为css
+
+// 针对js
+// 语法检查
+gulp.task('jshint', function () {
+	return gulp.src('index.js')
+			   .pipe(jshint())
+			   .pipe(jshint.reporter('default'));
+});
+// 合并文件之后压缩代码
+gulp.task('minify', function (){
+	return gulp.src('index.js')
+			   .pipe(concat('all.js'))
+			   .pipe(gulp.dest('js'))
+			   .pipe(uglify())
+			   .pipe(rename('min.js'))
+			   .pipe(gulp.dest('js'));
+});
+// 监视文件的变化
+gulp.task('watch', function () {
+	gulp.watch('index.js', ['jshint', 'minify']);
+});
+
+// 针对less
+// less转换为css并压缩
+gulp.task('testless', function () {
+	return gulp.src('index.less')
+	           .pipe(less())
+	           .pipe(uglify())
+	           .pipe(gulp.dest('css'));
+});
+// 注册缺省任务
+gulp.task('default', ['testless', 'jshint', 'minify', 'watch']);
+// gulp.task('default', ['jshint', 'minify']);
